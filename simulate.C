8c2a8ad040bb559dede70ff701151d9b16fc8df5
Filippo Mei <67705874+FelixofRivia@users.noreply.github.com>
2023-07-04 11:58:09 +0200

    create dir

--- simulate.C
+++ simulate.C
@@ -1,109 +1,15 @@
-
-
-
-/*
 void simulate(){
-    TChain* event = new TChain("data");
-    event->Add("./sample.root");
-    TRandom* r = new TRandom();
-    r->SetSeed(0);
-    TFile* outf = new TFile("./Data/run_5408/data_0000.root","recreate");
-    TTree* newtree = new TTree();
-    newtree->SetName("data");
-    int e = 15000;
-    bool run = true;
-    int k=0;
-    int N=0;
-    while(run){ 
-        
-        N = r->Integer(e-10000);
-        newtree=event->CopyTree("","",10000,N);
-        for (int i=0;i<10000;i++){} 
-        outf->cd();
-        newtree->Write("", TObject::kOverwrite);
-        outf->Close();
-        cout<<"done\n";
-        k++;
-        sleep(15);
-        outf = new TFile("./Data/run_5408/data_0000.root","recreate");
+
+    //make dir
+    try{
+        gSystem->Exec("mkdir Data");
+        gSystem->Exec("mkdir Data/run_005408");
     }
-}
-*/
-/*
-void simulate(){
-    TChain* event = new TChain("data");
-    event->Add("./sample.root");
-    TRandom* r = new TRandom();
-    r->SetSeed(0);
-    TFile* outf = new TFile("./Data/run_5408/data_0000.root","recreate");
-    TTree* newtree = new TTree();
-    newtree->SetName("data");
-    int e = 15000;
-    bool run = true;
-    int k=0;
-    int N=0;
-    N = r->Integer(e-10000);
-    newtree=event->CopyTree("","",10000,N);
-    outf->cd();
-    newtree->Write("", TObject::kOverwrite);
-    outf->Close();
-    cout<<"done\n";
-    k++;
-    sleep(15);
-    Long64_t t1=0;
-    Long64_t t2=0;
-    event->SetBranchAddress("evt_timestamp",&t1);
-    newtree->SetBranchAddress("evt_timestamp",&t2);
-    while(run){ 
-        outf = new TFile("./Data/run_5408/data_0000.root","recreate");
-        N = r->Integer(e-10000);
-        for (int i=0;i<10000;i++){
-            event->GetEntry(i+N);
-            t2=10;
-            newtree->Fill();
-        } 
-        outf->cd();
-        newtree->Write("", TObject::kOverwrite);
-        outf->Close();
-        cout<<"done\n";
-        k++;
-        sleep(15);
-    }
-}
-*/
-/*
-void simulate(){
-    TChain* event = new TChain("data");
-    event->Add("./sample.root");
+    catch(...){
+        cout<<"Directory exists";
+    } 
 
-    TFile* outf = new TFile("./Data/run_5408/data_0000.root","recreate");
-    TTree* newtree = event->CloneTree(0);
-    newtree->SetName("data");
-    int e = 15000;
-    bool run = true;
-    int k=0;
-    int steps=1000;
-    while(run){ 
-        k++;
-        for (int i=0;i<steps*k;i++){
-            event->GetEntry(i);
-            for (int n=0; n<10; n++){
-                newtree->Fill();
-            } 
-        } 
-        outf->cd();
-        newtree->Write("", TObject::kOverwrite);
-        outf->Close();
-        cout<<"done\t"<<k<<endl;
-        if (k==e/steps) break;
-        sleep(e/steps-k);
-        outf = new TFile("./Data/run_5408/data_0000.root","update");
-        newtree = event->CloneTree(0);
-    }
-}
-*/
 
-void simulate(){
     TChain* event = new TChain("data");
     event->Add("./sample.root");
 
